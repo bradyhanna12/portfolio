6e9695e6c812dccd15969998e0c028a8aa23540a
Brady Hanna <bradyhanna04@gmail.com>
2025-08-17 17:36:37 -0700

    update routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { NotFound } from "./pages/NotFound"
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/portfolio">
         <Routes>
           <Route index element={<Home></Home>}/>
           <Route path="*" element={<NotFound/>}/>
