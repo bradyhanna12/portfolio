1851280689a53bfbd2e859f167f5156966e0d436
Brady Hanna <bradyhanna04@gmail.com>
2025-11-28 16:59:13 -0800

    pictures to hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,6 +6,29 @@ export const HeroSection = () => {
                 id="hero" 
                 className="relative min-h-screen flex flex-col items-center justify-center px-4"
             >
+
+                {/* Background images */}
+                <img
+                    src="/images/me1.jpeg"
+                    alt="Background 1"
+                    className="absolute top-15 left-5 w-72 h-72 opacity-33 z-0 object-cover rounded-full"
+                />
+                <img
+                    src="/images/me2.jpeg"
+                    alt="Background 2"
+                    className="absolute hidden md:block top-15 right-45 w-80 h-80 opacity-33 z-0 object-cover rounded-full"
+                />
+                <img
+                    src="/images/me3.jpeg"
+                    alt="Background 3"
+                    className="absolute bottom-5 left-45 w-80 h-80 opacity-33 z-0 object-cover rounded-full"
+                />
+                <img
+                    src="/images/me4.jpeg"
+                    alt="Background 4"
+                    className="absolute hidden md:block bottom-10 right-10 w-72 h-72 opacity-33 z-0 object-cover rounded-full"
+                />
+
                 <div className="container max-w-4xl mx-auto text-center z-10">
                     <div className="space-y-6">
                         <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
